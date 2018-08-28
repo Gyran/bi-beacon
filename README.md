20b36d615c03aa0acb6f5844f3a02976c1d01b4d
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2018-08-28 14:45:07 +0200

    this repo only controls the bi beacon

--- README.md
+++ README.md
@@ -1,28 +1,16 @@
 # BI beacon
-Small wrapper around the api for bi beacon. More info can be found at https://bi-beacon.readthedocs.io/en/latest/code_examples.html
+Small wrapper around the api for bi beacon. More info can be found at https://bi-beacon.readthedocs.io/en/latest/api.html
 
 More info about the BI beacon can be found at https://bi-beacon.se/
 
 ## Usage
 
-### Control
 ```
-const { BIBeaconController: Controller } = require('bi-beacon');
+const BIBeacon = require('bi-beacon');
 
-const beacon1 = new BIBeaconController('beacon1');
-const beacon2 = new BIBeaconController('beacon2');
+const beacon1 = new BIBeacon('beacon1');
+const beacon2 = new BIBeacon('beacon2');
 
 beacon1.color('#F0F0F0');
 beacon2.pulse('#0F0F0F', 1000);
-```
-
-### Client
-```
-const { BIBeaconClient: Client } = require('bi-beacon');
-
-const beacon1 = new BIBeaconClient('beacon1');
-beacon1.on('command', (data) => {
-  console.log('got data', data);
-});
-beacon1.connect();
 ```
--- index.js
+++ index.js
@@ -1,7 +1,72 @@
-const Controller = require('./controller');
-const Client = require('./client');
+const https = require('https');
+const querystring = require('querystring');
 
-module.exports = {
-  Controller,
-  Client,
-};
+class BIBeacon {
+
+  constructor(systemid, {
+    host = 'api.cilamp.se',
+    port = 443,
+  } = {}) {
+    this._systemid = systemid;
+    this._host = host;
+    this._port = port;
+  }
+
+  color(color) {
+    return this._post({ color });
+  }
+
+  pulse(color, period) {
+    return this._post({ color, period });
+  }
+
+  async _post(data) {
+    const path = `/v1/${ this._systemid }/`;
+    const postData = querystring.stringify(data);
+
+    return new Promise((resovle, reject) => {
+      let responseData = '';
+
+      const request = https.request(
+        {
+          method: 'post',
+          port: this._port,
+          host: this._host,
+          path,
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+          },
+        },
+        response => {
+          response.on('data', chunk => {
+            responseData += chunk;
+          });
+
+          // The whole response has been received
+          response.on('end', () => {
+            try {
+              const out = JSON.parse(responseData);
+
+              if (response.statusCode === 200) {
+                resovle(out);
+              } else {
+                reject(out);
+              }
+            } catch (error) {
+              reject(error);
+            }
+          });
+        },
+      );
+
+      request.on('error', error => {
+        reject(error);
+      });
+      request.write(postData);
+      request.end();
+    });
+  }
+
+}
+
+module.exports = BIBeacon;
